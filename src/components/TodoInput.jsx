9f937db5b274faa68c4b69a2a77521804f811493
CyberTycoon <silasokanla200@gmail.com>
2024-12-14 07:55:51 -0600

    wrap

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -6,7 +6,7 @@ export default function TodoInput(props) {
     const { handleAddTodos, todoValue, setTodoValue} = props 
     
     return (
-    <header>
+    <header className=''>
         <input value={todoValue} onChange={(e) => {
             setTodoValue(e.target.value)
         }} type="text" placeholder='Enter todo....' />
